b72f3d3267c4db41c9d71b3c8739451c02257b7a
Pedro Santiago <psantia2@live.com>
2023-10-16 17:46:47 -0400

    Initiated Git and Pushed project to GitHub

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int num1 = 20;
+        double num2 =20.1;
+        
+        String outcome= (num1>num2)? "Num1 is greater" : "Num2 is greater";
+        System.out.println(outcome);
+    }
+}
